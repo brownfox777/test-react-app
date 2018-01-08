09ad4e8d0b73f3fae2a393cdb3d333fcaeaaeff0
brownfox777 <brownfox777@gmail.com>
2018-01-08 12:40:40 +0300

    add Person component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
+  render = () => {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, it's test-react-app</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, it\'s test-react-app'))
   }
 }
 
