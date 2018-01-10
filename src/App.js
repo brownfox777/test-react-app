b7ce8c4e7c6bfc0487ba6de391b4a4ecabce0302
brownfox777 <brownfox777@gmail.com>
2018-01-10 11:03:40 +0300

    methods with setState improved

--- src/App.js
+++ src/App.js
@@ -6,37 +6,105 @@ class App extends Component {
 
   state = {
     persons: [
-      { id: 0, name: 'Max', age: 29, hobbie: 'Racing' },
       { id: 1, name: 'Marussya', age: 66, hobbie: 'Skying' },
+      { id: 0, name: 'Max', age: 29, hobbie: 'Racing' },
       { id: 2, name: 'Victor', age: 22, hobbie: '' }
     ],
-    otherState: 'some other state'
+    otherState: 'some other state',
+    showPersons: false
   }
 
-  switchNameHandler = () => {
-    this.setState({
-      persons: [
-        { id: 0, name: 'Max', age: 29, hobbie: 'Racing' },
-        { id: 1, name: 'Masha', age: 222, hobbie: 'Skying' },
-        { id: 2, name: 'Bob', age: 22, hobbie: 'Drinking' }
-      ]
-    })
+  switchNameHandler = (newName) => {
+
+    const persons = [...this.state.persons];
+    persons[0].name = newName;
+
+    this.setState({ persons: persons });
+  }
+
+  deletePersonHandler = (index) => {
+    //let persons = this.state.persons; 
+    // массив это ссылочный тип и поэтому работая с массивом persons мы изменяем сразу и this.state.persons
+    // let persons = this.state.persons.slice(); 
+    //Безопаснее создавать новый абсолютно массив хэлпером slice() без параметров
+    let persons = [...this.state.persons];
+    //или используя спред оператор
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
+    //всегда следует менять стэйт не напрямую а создавая копию, изменяя ее и потом this.setState({ someState: copy })
+  };
+
+  nameChangeHandler = (event, id) => {
+
+    const personId = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personId]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personId] = person;
+
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons })
   }
 
   listPersons = () => {
     const persons = this.state.persons || [];
-    return persons.map((person) => {
-    return (<Person key={person.id} name={person.name} age={person.age}>My Hobbies: {person.hobbie}</Person>)
+    return persons.map((person, index) => {
+    return (
+      <Person 
+      key={person.id} 
+      name={person.name} 
+      age={person.age}
+      // click={this.switchNameHandler.bind(this, 'MaxMax')}
+      click={() => this.deletePersonHandler(index)}
+      changed={(event) => this.nameChangeHandler(event, person.id)}>
+      My Hobbies: {person.hobbie}
+      </Person>)
     })
   }
 
   render = () => {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      display: 'block',
+      margin: '16px auto'
+    };
+
+    let persons = this.state.showPersons 
+      ? this.listPersons() 
+      : null
+
     return (
       <div className="App">
+
         <h1>Hi, it's test-react-app</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        { this.listPersons() }
+
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian')}
+        >Switch Name
+        </button>
+
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}
+        >Toggle Persons
+        </button>
+
+        {persons}
+
       </div>
       );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
-      <p>I'm { props.name }! And I'm { props.age } years old!</p>
+    <div className="Person">
+      <p onClick={props.click}>I'm { props.name }! And I'm { props.age } years old!</p>
       <p>{ props.children }</p>
+      <input onChange={props.changed} value={props.name}/>
     </div>    
   );
 };
